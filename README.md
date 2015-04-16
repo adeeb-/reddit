4e7abd127285f3cb88b4c9d6d6d0ef426cc15719
adeeb_ <adeeb.rahman@gmail.com>
2015-04-16 19:39:00 +0000

    Finally finished and works

---
+++ README.md
@@ -0,0 +1,6 @@
+A reddit clone written in rails.
+
+
+rails g scaffold Link title:string url:string
+
+rails g migration add_user_id_to_links user_id:integer:index
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  resources :comments
+
+  devise_for :users
+  
+  resources :links do
+  member do
+    put "like", to: "links#upvote"
+    put "dislike", to: "links#downvote"
+  end
+  end
+  
+  resources :comments
+  
+  root "links#index"
+
+end
